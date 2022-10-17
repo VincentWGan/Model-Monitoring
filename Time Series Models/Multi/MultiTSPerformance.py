919aeb089a61f4f4f3a479f4380c3afe2f7b8ad7
WenqinGan <wxg39@psu.edu>
2022-10-17 16:07:05 -0400

    deleted progree report folder

--- Time Series Models/Multi/MultiTSPerformance.py
+++ Time Series Models/Multi/MultiTSPerformance.py
@@ -5,6 +5,7 @@ import psutil
 import collections
 import math
 from sklearn.metrics import r2_score
+from collections import deque
 
 class multiTSModelPerformance(object):
 
@@ -15,7 +16,7 @@ class multiTSModelPerformance(object):
         assert batch_size <= np.shape(y_pred)[0], "batch_size must be smaller than length of y."
         assert np.shape(y_pred)[0] >= 2, "The length of y_pred must be at least 2."
         assert np.shape(y_true)[0] >= 2, "The length of y_pred must be at least 2."
-        assert interval >= 0, "frequency must be positive"
+        assert interval >= 0, "Interval must be positive"
         
 
         # store the inputs if they are valid
@@ -24,12 +25,15 @@ class multiTSModelPerformance(object):
         self.batch_size = batch_size
         self.interval = interval
         
+    def addData(self, new_y_pred, new_y_true):
+        pass
         
     def showPlots(self):
         scores = {'r2': [], 'mse': [], 'mape': []}
         fig, ((ax1, ax2), (ax3, ax4))= plt.subplots(2, 2)
         def helper(i):
-            
+            y_pred_batch = 
+            scores['r2'].append(r2_score(self.y_true, self.y_pred))
                 
         plot = FuncAnimation(fig, helper, interval=self.interval)
         plt.show()
